50ad163ef79450884cf8543d07766c697b21365a
kamalakarrao <kamalakar.sambaraj@gmail.com>
2018-01-11 07:59:23 +0530

    Bluetooth Functionality Yet to be added

--- app/src/main/java/com/hksapps/experiencia/MainActivity.java
+++ app/src/main/java/com/hksapps/experiencia/MainActivity.java
@@ -1,5 +1,7 @@
 package com.hksapps.experiencia;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,22 +10,32 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
+
+import app.akexorcist.bluetotohspp.library.BluetoothSPP;
+import app.akexorcist.bluetotohspp.library.BluetoothState;
+import app.akexorcist.bluetotohspp.library.DeviceList;
 
 public class MainActivity extends AppCompatActivity {
 
-    /*   private BluetoothSPP bt;
+    private BluetoothSPP bt;
 
-       @Override
-       protected void onStart() {
-           super.onStart();
-           if(!bt.isBluetoothEnabled()) {
-               // Do somthing if bluetooth is disable
-               bt.enable();
-           } else {
-               // Do something if bluetooth is already enable
-           }
-       }
-   */
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if (!bt.isBluetoothEnabled()) {
+            // Do somthing if bluetooth is disable
+            bt.enable();
+            //   bt.autoConnect("Experiencia");
+            //   connectToBluetooth();
+        } else {
+            // Do something if bluetooth is already enable
+            // connectToBluetooth();
+
+
+        }
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,9 +43,9 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-       //  bt = new BluetoothSPP(this);
+        bt = new BluetoothSPP(this);
 
-
+     //   connectToBluetooth();
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -43,6 +55,56 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+    }
+
+
+    private void connectToBluetooth() {
+
+        bt.startService(BluetoothState.DEVICE_OTHER);
+
+        //  Toast.makeText(this, bt.getConnectedDeviceName().toString(), Toast.LENGTH_SHORT).show();
+
+        String[] arr = bt.getPairedDeviceName();
+
+        for (int i = 0; i < arr.length; i++) {
+
+            Toast.makeText(this, arr[i], Toast.LENGTH_SHORT).show();
+
+        }
+
+        //  bt.autoConnect("Experiencia");
+
+      /*  if(bt.getPairedDeviceName().equals("Experiencia")){
+
+
+
+        }else {
+
+bt.connect("Experiencia");*/
+
+//            Toast.makeText(this, "12345"+bt.getConnectedDeviceName().toString(), Toast.LENGTH_SHORT).show();
+
+        Intent intent = new Intent(getApplicationContext(), DeviceList.class);
+        startActivityForResult(intent, BluetoothState.REQUEST_CONNECT_DEVICE);
+
+        /*}*/
+
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == BluetoothState.REQUEST_CONNECT_DEVICE) {
+            if (resultCode == Activity.RESULT_OK) {
+                bt.connect(data);
+            }
+        } else if (requestCode == BluetoothState.REQUEST_ENABLE_BT) {
+            if (resultCode == Activity.RESULT_OK) {
+                bt.setupService();
+                bt.startService(BluetoothState.DEVICE_OTHER);
+                //setup();
+            } else {
+                // Do something if user doesn't choose any device (Pressed back)
+            }
+        }
     }
 
     @Override
