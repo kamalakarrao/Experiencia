fdf05bca99e17f2ffa47e1371fca0df5cf3693e9
kamalakarrao <kamalakar.sambaraj@gmail.com>
2018-01-20 12:58:34 +0530

    Bluetooth Functionality

--- app/src/main/java/com/hksapps/experiencia/MainActivity.java
+++ app/src/main/java/com/hksapps/experiencia/MainActivity.java
@@ -1,10 +1,10 @@
 package com.hksapps.experiencia;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -36,6 +36,10 @@ public class MainActivity extends AppCompatActivity {
         setup_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
+                Intent i = new Intent(MainActivity.this,BluetoothActivity.class);
+
+                startActivity(i);
                 
             }
         });
